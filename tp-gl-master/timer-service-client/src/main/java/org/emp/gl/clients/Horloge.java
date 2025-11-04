2648449072fe48180fc79abf2cfc0f39ff8870f8
Souha <souha@example.com>
2025-11-04 10:17:42 +0100

    Mise à jour de la classe Horloge

--- tp-gl-master/timer-service-client/src/main/java/org/emp/gl/clients/Horloge.java
+++ tp-gl-master/timer-service-client/src/main/java/org/emp/gl/clients/Horloge.java
@@ -1,26 +1,30 @@
-package org.emp.gl.clients ; 
+package org.emp.gl.clients;
 
-import org.emp.gl.timer.service.TimerService ; 
+import org.emp.gl.timer.service.TimerChangeListener;
+import org.emp.gl.timer.service.TimerService;
 
+public class Horloge implements TimerChangeListener {
 
-public class Horloge {
+    private final String nom;
+    private final TimerService timerService;
 
-    String name; 
-    TimerService timerService ; 
+    public Horloge(String nom, TimerService timerService) {
+        this.nom = nom;
+        this.timerService = timerService;
 
-
-    public Horloge (String name) {
-        this.name = name ; 
-
-        System.out.println ("Horloge "+name+" initialized!") ;
+        // inscription en tant qu'observateur du TimerService
+        this.timerService.addTimeChangeListener(this);
     }
 
-    public void afficherHeure () {
-        if (timerService != null)
-            System.out.println (name + " affiche " + 
-                                timerService.getHeures() +":"+
-                                timerService.getMinutes()+":"+
-                                timerService.getSecondes()) ;
+    @Override
+    public void propertyChange(String prop, Object oldValue, Object newValue) {
+        // réagir uniquement aux changements de secondes
+        if (SECONDE_PROP.equals(prop)) {
+            System.out.printf("[%s] Heure : %02d:%02d:%02d%n",
+                    nom,
+                    timerService.getHeures(),
+                    timerService.getMinutes(),
+                    timerService.getSecondes());
+        }
     }
-
 }
