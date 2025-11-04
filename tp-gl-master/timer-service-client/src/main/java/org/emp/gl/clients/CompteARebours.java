a692b3c544023a20d0d2d20b250d0751932c8230
Souha <souha@example.com>
2025-11-04 10:52:53 +0100

    Mise a jour de la classe CompteARebours

--- tp-gl-master/timer-service-client/src/main/java/org/emp/gl/clients/CompteARebours.java
+++ tp-gl-master/timer-service-client/src/main/java/org/emp/gl/clients/CompteARebours.java
@@ -1,4 +1,37 @@
 package org.emp.gl.clients;
 
-public class CompteARebours {
+import org.emp.gl.timer.service.TimerChangeListener;
+import org.emp.gl.timer.service.TimerService;
+
+import java.util.Random;
+
+public class CompteARebours implements TimerChangeListener {
+
+    private final TimerService timerService;
+    private int compteur;
+    private final String nom;
+
+    public CompteARebours(String nom, int compteur, TimerService timerService) {
+        this.nom = nom;
+        this.compteur = compteur;
+        this.timerService = timerService;
+
+        // s'inscrire comme observateur
+        this.timerService.addTimeChangeListener(this);
+    }
+
+    @Override
+    public void propertyChange(String prop, Object oldValue, Object newValue) {
+        if (TimerChangeListener.SECONDE_PROP.equals(prop)) {
+            if (compteur > 0) {
+                System.out.printf("[%s] Compte à rebours : %d%n", nom, compteur);
+                compteur--;
+            }
+            if (compteur == 0) {
+                // se désinscrire automatiquement
+                timerService.removeTimeChangeListener(this);
+                System.out.printf("[%s] Terminé !\n", nom);
+            }
+        }
+    }
 }
